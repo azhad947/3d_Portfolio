e6759128c9f6023738d485984e23c205f0cb17e2
Azhad batt <izhadkhan246@gmail.com>
2023-06-29 00:34:40 +0530

    sec

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -45,7 +45,7 @@ const BallCanvas = ({ icon }) => {
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls  enableZoom={false} />
+        <OrbitControls enableZoom={false} />
         <Ball imgUrl={icon} />
       </Suspense>
 
@@ -54,4 +54,4 @@ const BallCanvas = ({ icon }) => {
   );
 };
 
-export default  BallCanvas;
+export default BallCanvas;
